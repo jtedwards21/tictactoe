faef3910615796a5321796c1e21d0dcdf75ba1e5
James Ahlm <14782828428@qq.com>
2016-12-04 10:51:08 +0800

    Added clicks for squares and cursors

--- public/javascripts/components/game.jsx
+++ public/javascripts/components/game.jsx
@@ -6,27 +6,42 @@ export default class Game extends React.Component {
 
     this.state = {
       playerTurn: true
-      boardState: [["","",""],["","",""],["","",""]]
+      board: [["","",""],["","",""],["","",""]]
+      playerPiece: 'X',
+      computerPiece: 'O',
     };
   }
+  handleClick(row, column){
+    var board = this.state.board;
+    if(board[row][column] == ""){
+	board[row][column] = this.state.playerPiece;
+	this.setState({board: board});
+    }
+  }
+  
   render() {
+
+    var squareStyle;
+
+    if (this.state.playerTurn = true){squareStyle = {cursor: "pointer"};} else {squareStyle = {cursor: "unset"};}
+
     return (
       <div className="box">
 	<div className="board">
 	  <div id="top-row" className="board-row">
-	    <div className="text-center square col-xs-4">{this.state}</div>
-	    <div className="text-center square col-xs-4"></div>
-	    <div className="text-center square col-xs-4"></div>
+	    <div style={squareStyle} className="text-center square col-xs-4" onClick={this.handleClick(0,0)}>{this.state.board[0][0]}</div>
+	    <div style={squareStyle} className="text-center square col-xs-4" onClick={this.handleClick(0,1)}>{this.state.board[0][1]}</div>
+	    <div style={squareStyle} className="text-center square col-xs-4" onClick={this.handleClick(0,2)}>{this.state.board[0][2]}</div>
 	  </div>
 	  <div id="middle-row" className="board-row">
-	    <div className="text-center square col-xs-4"></div>
-	    <div className="text-center square col-xs-4"></div>
-	    <div className="text-center square col-xs-4"></div>
+	    <div style={squareStyle} className="text-center square col-xs-4" onClick={this.handleClick(1,0)}>{this.state.board[1][0]}</div>
+	    <div style={squareStyle} className="text-center square col-xs-4" onClick={this.handleClick(1,1)}>{this.state.board[1][1]}</div>
+	    <div style={squareStyle} className="text-center square col-xs-4" onClick={this.handleClick(1,2)}>{this.state.board[1][2]}</div>
 	  </div>
 	  <div id="botton-row" className="board-row">
-	    <div className="text-center square col-xs-4"></div>
-	    <div className="text-center square col-xs-4"></div>
-	    <div className="text-center square col-xs-4"></div>
+	    <div style={squareStyle} className="text-center square col-xs-4"　onClick={this.handleClick(2,0)}>{this.state.board[2][0]}</div>
+	    <div style={squareStyle} className="text-center square col-xs-4"　onClick={this.handleClick(2,1)}>{this.state.board[2][1]}</div>
+	    <div style={squareStyle} className="text-center square col-xs-4"　onClick={this.handleClick(2,2)}>{this.state.board[2][2]}</div>
 	  </div>
 	</div>
       </div>
